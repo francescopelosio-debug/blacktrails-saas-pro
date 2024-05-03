693be9b4f103fea3da1189838fa93d8304a74dd1
Pagebakers <eelco@appulse.nl>
2024-05-03 20:17:50 +0100

    fix: fixed issue where beacon styles would not apply correctly

---
+++ .changeset/cool-falcons-hear.md
@@ -0,0 +1,6 @@
+---
+'@saas-ui-pro/onboarding': patch
+'@saas-ui-pro/react': patch
+---
+
+Fixed issue where Beacon styles would not apply correctly
--- saas-ui/onboarding/src/beacon.tsx
+++ saas-ui/onboarding/src/beacon.tsx
@@ -1,64 +1,20 @@
 import * as React from 'react'
 
 import {
-  chakra,
-  forwardRef,
-  keyframes,
-  useStyleConfig,
   HTMLChakraProps,
   ThemingProps,
-  useTheme,
-  SystemStyleObject,
+  chakra,
+  forwardRef,
+  useStyleConfig,
 } from '@chakra-ui/react'
-
 import { cx } from '@chakra-ui/utils'
-
-const pulse = keyframes`
-  0% {
-    transform: scale(1);
-    opacity: 1;
-  }
-  55% {
-    transform: scale(1.6);
-    opacity: 0;
-  }
-  100% {
-    opacity: 0;
-  }
-`
 
 export interface BeaconProps
   extends HTMLChakraProps<'div'>,
     ThemingProps<'SuiBeacon'> {}
 
-const defaultStyleConfig: SystemStyleObject = {
-  baseStyle: {
-    bg: 'green.400',
-    borderRadius: '50%',
-    h: '4',
-    w: '4',
-    _before: {
-      content: '""',
-      d: 'block',
-      w: '4',
-      h: '4',
-      animation: `${pulse} 1s ease-in-out infinite`,
-      boxShadow: '0 0 2px 2px',
-      color: 'green.400',
-      borderRadius: '50%',
-    },
-  },
-}
-
 export const Beacon = forwardRef<BeaconProps, 'div'>((props, ref) => {
-  const theme = useTheme()
-
-  const styleConfig = theme.components.SuiBeacon || defaultStyleConfig
-
-  const styles = useStyleConfig('SuiBeacon', {
-    styleConfig,
-    ...props,
-  })
+  const styles = useStyleConfig('SuiBeacon', props)
 
   return (
     <chakra.div
