d9cf4cf1b8553bacf9846ad56cbfb9525daf939b
Eelco Wiersma <eelco@appulse.nl>
2024-10-25 18:01:59 +0100

    chore: update build

--- packages/cli/lib/cli.js
+++ packages/cli/lib/cli.js
@@ -1,7 +1,2 @@
 #!/usr/bin/env node
-import { run as i } from '@stricli/core'
-
-import { b as o, a as r } from './chunk-XVSCTFM7.js'
-
-import { run as i } from '@stricli/core'
-await i(r, process.argv.slice(2), o(process))
+import{a as r,b as o}from"./chunk-XVSCTFM7.js";import{run as i}from"@stricli/core";await i(r,process.argv.slice(2),o(process));
--- packages/cli/lib/impl-VDXYRJRU.js
+++ packages/cli/lib/impl-VDXYRJRU.js
@@ -1,5 +1,1 @@
-import './chunk-5V5IBQUV.js'
-import { i as a, j as b, k as c } from './chunk-UIMRDWOQ.js'
-
-import './chunk-5V5IBQUV.js'
-export { b as init, a as initOptionsSchema, c as runInit }
+import{i as a,j as b,k as c}from"./chunk-UIMRDWOQ.js";import"./chunk-5V5IBQUV.js";export{b as init,a as initOptionsSchema,c as runInit};
