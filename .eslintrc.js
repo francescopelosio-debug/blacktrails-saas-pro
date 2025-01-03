7ac8e2f86f6e3bae6b2afbe002ce50a66df24dd4
Eelco Wiersma <eelco@appulse.nl>
2025-01-03 10:58:07 +0000

    chore: add react linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,8 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'prettier',
     'plugin:import/typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
   ],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
@@ -39,5 +41,6 @@ module.exports = {
         ],
       },
     ],
+    'react/prop-types': 'off',
   },
 }
